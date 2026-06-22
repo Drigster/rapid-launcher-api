e57006677808fc9f4dac91f27301db57367ba9c8
Gabriel Jõe <62962772+Drigster@users.noreply.github.com>
2026-06-22 23:44:24 +0300

    add serverUserData endpoint

--- src/routes/server.ts
+++ src/routes/server.ts
@@ -22,6 +22,11 @@ const getServersSchema = v.object({
 	access_token: v.string(),
 });
 
+const getServerUserSchema = v.object({
+	access_token: v.string(),
+	server_id: v.string(),
+});
+
 router.post("/joinServer", async (req, res) => {
 	const data = v.safeParse(joinServerSchema, req.body);
 
@@ -101,7 +106,7 @@ router.post("/joinServer", async (req, res) => {
 			return res.status(200).json({ success: true });
 		}
 
-		return res.status(200).json({ error: "Игрок уже находится на сервере" });
+		return res.status(403).json({ error: "Игрок уже находится на сервере" });
 	} catch (e) {
 		console.error(e);
 		return res.status(500).json({ error: "Ошибка сервера, попробуйте позже" });
@@ -155,6 +160,51 @@ router.post("/getServers", async (req, res) => {
 	return res.status(200).json({ success: true, servers });
 });
 
+router.post("/getServerUser", async (req, res) => {
+	const data = v.safeParse(getServerUserSchema, req.body);
+
+	if (!data.success) {
+		return res.status(400).json({ errors: data.issues });
+	}
+
+	const { session, user } = await validateSessionToken(
+		data.output.access_token,
+	);
+
+	if (!session || !user) {
+		return res
+			.status(403)
+			.json({ error: "Сессия устарела или недействительна" });
+	}
+
+	const server = await db.selectFrom("Server").selectAll().where("id", "=", data.output.server_id as any).executeTakeFirst();
+
+	if (server == undefined) {
+		return res.status(200).json({ success: false });
+	}
+
+	const callback_url = URL.parse(server.callback_url);
+	if (!callback_url) {
+		return res.status(200).json({ success: false });
+	}
+
+	callback_url.pathname = posix.join(callback_url.pathname, "getUser");
+	try {
+		const response = await axios.post(callback_url.href, {
+			user_id: user.id,
+		});
+
+		if (response.data?.success != true) {
+			return res.status(200).json({ success: false });
+		}
+
+		return res.status(500).json({ success: true, userData: response.data.userData });
+	} catch (e) {
+		console.error(e);
+		return res.status(500).json({ error: "Ошибка сервера, попробуйте позже" });
+	}
+});
+
 // TEMP: Server API dummy
 router.post("/checkUser", async (req, res) => {
 	if (req.body.username == "test") {
@@ -164,12 +214,18 @@ router.post("/checkUser", async (req, res) => {
 	return res.status(200).json({ success: false });
 });
 
+const ONE_HOUR = 1000 * 60 * 60;
+const ONE_DAY = ONE_HOUR * 24;
 router.post("/getUser", async (req, res) => {
-	if (req.body.username == "test") {
-		return res.status(200).json({ success: true });
+	const userData = {
+		playTimeSeconds: 458700,
+		lastPlayedTimestamp: new Date(),
+		subscription: "Платина",
+		subscriptionExpiresAt: new Date(Date.now() + ONE_DAY * 14 + ONE_HOUR * 20),
+		credits: 1000
 	}
 
-	return res.status(200).json({ success: false });
+	return res.status(200).json({ success: true, userData: userData });
 });
 
 export default router;
